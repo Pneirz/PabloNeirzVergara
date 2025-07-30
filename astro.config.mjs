70f67479d94b8e7d676f83c07f81a5a17248c4fc
Pablo Neirz <pablo.neirz.vergara@gmail.com>
2025-07-30 18:24:01 -0400

    favicon error

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://Pneirz.github.io',
-	base: '/PabloNeirzVergara',
+	base: '/PabloNeirzVergara/',
 	vite: {
 		plugins: [tailwindcss()]
 	}
