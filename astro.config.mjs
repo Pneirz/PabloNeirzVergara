c5d3ccb67db085b929ad69f320610ef309d7d36d
Pablo Neirz <pablo.neirz.vergara@gmail.com>
2025-07-30 16:45:56 -0400

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://Pneirz.github.io',
-	base: '/p-pneirz',
+	base: '/PabloNeirzVergara',
 	vite: {
 		plugins: [tailwindcss()]
 	}
