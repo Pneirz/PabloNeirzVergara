99bd06e6304b61a7009c648322c5a2f0a8f79764
Pablo Neirz <pablo.neirz.vergara@gmail.com>
2025-08-19 14:58:04 -0400

    mejoras de TW

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,15 @@
-import tailwindcss from 'tailwindcss';
+import typography from '@tailwindcss/typography';
+import forms from '@tailwindcss/forms';
 
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    container: { center: true, padding: '1rem' },
+    extend: {
+      borderRadius: { 'inherit': 'inherit' },
+      boxShadow: { card: '0 2px 4px -1px rgb(0 0 0 / 0.06), 0 4px 6px -1px rgb(0 0 0 / 0.05)' },
+      transitionTimingFunction: { 'in-out-soft': 'cubic-bezier(.4,0,.2,1)' },
+    },
+  },
+  plugins: [typography, forms],
 };
